3ab4bb7233289581441a82707d49e80b57c5f80e
TakT <kirill.bereza@yandex.ru>
2016-03-26 19:08:44 +0600

    Added index page anim

--- assets/js/template.js
+++ assets/js/template.js
@@ -362,3 +362,15 @@ function calFirstScreen(windowWidth) {
 		}
 	});
 })(jQuery);
+
+$.fn.extend({
+	animateCss: function(animationName, callback) {
+		var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+		$(this).addClass('animated ' + animationName).one(animationEnd, function() {
+			$(this).removeClass('animated ' + animationName);
+			if (callback && callback != undefined) {
+				callback();
+			}
+		});
+	}
+});
